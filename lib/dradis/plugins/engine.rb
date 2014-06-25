89cdd69e90c02d0d5ad66f52ab10674b8cd7e2b0
Daniel Martin <etd-github@nomejortu.com>
2014-06-25 09:55:36 +0200

    Initial import of the project

---
+++ lib/dradis/plugins/engine.rb
@@ -0,0 +1,19 @@
+module Dradis
+  module Plugins
+    class Engine < ::Rails::Engine
+      isolate_namespace Dradis::Plugins
+
+      # initializer 'frontend.append_migrations' do |app|
+      #   unless app.root.to_s == root.to_s
+      #     config.paths["db/migrate"].expanded.each do |path|
+      #       app.config.paths["db/migrate"].push(path)
+      #     end
+      #   end
+      # end
+
+      # initializer 'frontend.asset_precompile_paths' do |app|
+      #   app.config.assets.precompile += ["dradis/frontend/manifests/*"]
+      # end
+    end
+  end
+end
