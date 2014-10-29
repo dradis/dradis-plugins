97d63ce80082897e327bfa8ab54936c7871033ec
Daniel Martin <etd-github@nomejortu.com>
2014-10-29 16:04:09 +0100

    Import / Result - for an :id field so we can populate the plugin_id field

--- lib/dradis/plugins/import/result.rb
+++ lib/dradis/plugins/import/result.rb
@@ -1,11 +1,12 @@
 module Dradis::Plugins::Import
   class Result
-    attr_accessor :description, :tags, :title
+    attr_accessor :description, :id, :tags, :title
 
     def initialize(args={})
       @description = args[:description] || "The Import plugin didn't provide a :description for this result."
-      @tags = args[:tags] || []
-      @title = args[:description] || "The Import plugin didn't provide a :title for this result."
+      @id          = args[:id]          || "The Import plugin didn't provide an :id for this result."
+      @tags        = args[:tags]        || []
+      @title       = args[:title]       || "The Import plugin didn't provide a :title for this result."
     end
   end
 end
