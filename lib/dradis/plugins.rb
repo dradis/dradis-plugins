e9aaccdcab77a79343c64a555f50ed6faac64d5b
Daniel Martin <etd-github@nomejortu.com>
2014-11-06 16:06:48 +0100

    Extract Plugins::Base to a separate file

--- lib/dradis/plugins.rb
+++ lib/dradis/plugins.rb
@@ -4,6 +4,7 @@ require 'dradis/plugins/version'
 require 'dradis/plugins/content_service'
 require 'dradis/plugins/template_service'
 
+require 'dradis/plugins/base'
 require 'dradis/plugins/export'
 require 'dradis/plugins/import'
 require 'dradis/plugins/upload'
@@ -12,47 +13,6 @@ require 'dradis/plugins/templates'
 
 module Dradis
   module Plugins
-    module Base
-      def self.included(base)
-        base.extend ClassMethods
-        base.class_eval do
-          # mattr_accessor :plugin_name
-
-          @features = []
-          @name = 'Use plugin_info(args) with :name to provide a name for this plugin.'
-          Plugins::register(self)
-        end
-
-        # Extend the engine with other functionality
-        base.send :include, Dradis::Plugins::Templates
-      end
-
-      module ClassMethods
-        def description(new_description)
-          @description = new_description
-        end
-
-        def plugin_description
-          @description ||= "This plugin doesn't provide a :description"
-        end
-
-        def plugin_name
-          self.name.split('::')[2].underscore.to_sym
-        end
-
-        def provides(*list)
-          @features = list
-          if list.include?(:upload)
-            include Dradis::Plugins::Upload::Base
-          end
-        end
-
-        def provides?(feature)
-          @features.include?(feature)
-        end
-      end
-    end
-
     class << self
       @@extensions = []
 
