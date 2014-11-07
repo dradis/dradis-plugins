e324ba47f6e1fb4b9682f2c094437c33ac3fa520
Daniel Martin <etd-github@nomejortu.com>
2014-11-07 16:46:24 +0100

    ContentService - manage validation errors by add-on writers
    Do not trust the add-on writer to provide valid models, account for the
    possibility of AR validation errors on creation.
    
    Also, some times a plugin is too keen a provides too much data as
    Evidence. Try to manage that via length restrictions and warnings to the
    user.

--- lib/dradis/plugins/content_service.rb
+++ lib/dradis/plugins/content_service.rb
@@ -58,11 +58,16 @@ module Dradis
       #
 
       def create_node(args={})
-        label   = args[:label] || "create_node() invoked by #{plugin} without a :label parameter"
-        parent  = args[:parent] || default_parent_node
+        label    = args[:label]  || "create_node() invoked by #{plugin} without a :label parameter"
+        parent   = args[:parent] || default_parent_node
+
         type_id = begin
-          tmp_type = args[:type].to_s.upcase
-          class_for(:node)::Types::const_defined?(tmp_type) ? "#{class_for(:node)::Types}::#{tmp_type}".constantize : default_node_type
+          if args[:type]
+            tmp_type = args[:type].to_s.upcase
+            class_for(:node)::Types::const_defined?(tmp_type) ? "#{class_for(:node)::Types}::#{tmp_type}".constantize : default_node_type
+          else
+            default_node_type
+          end
         end
 
         # FIXME: how ugly is this?
@@ -77,7 +82,14 @@ module Dradis
         node = args[:node] || default_parent_node
         text = args[:text] || "create_note() invoked by #{plugin} without a :text parameter"
 
-        node.notes.create text: text, category: default_note_category, author: default_author
+        note = node.notes.new(text: text, category: default_note_category, author: default_author)
+        if note.valid?
+          note.save
+        else
+          try_rescue_from_length_validation(model: note, field: :text, text: text, msg: 'Error in create_note()')
+        end
+
+        note
       end
 
       def create_issue(args={})
@@ -94,10 +106,16 @@ module Dradis
         text << "\n\n#[plugin]#\n#{uuid[0]}\n"
         text << "\n\n#[plugin_id]#\n#{uuid[1]}\n"
 
-        issue = class_for(:issue).create(text: text) do |i|
+        issue = class_for(:issue).new(text: text) do |i|
           i.author   = default_author
           i.node     = issuelib
           i.category = default_issue_category
+        end
+
+        if issue.valid?
+          issue.save
+        else
+          try_rescue_from_length_validation(model: issue, field: :text, text: text, msg: 'Error in create_issue()')
         end
 
         issue_cache[cache_key] = issue
@@ -108,7 +126,13 @@ module Dradis
         node    = args[:node] || default_parent_node
         issue   = args[:issue] || create_issue(text: "#[Title]#\nAuto-generated issue.\n\n#[Description]#\ncreate_evidence() invoked by #{plugin} without an :issue parameter")
 
-        node.evidence.create(issue_id: issue.id, content: content)
+        evidence = node.evidence.new(issue_id: issue.id, content: content)
+        if evidence.valid?
+          evidence.save
+        else
+          try_rescue_from_length_validation(model: evidence, field: :content, text: content, msg: 'Error in create_evidence()')
+        end
+        evidence
       end
 
 
@@ -141,6 +165,33 @@ module Dradis
 
       def issuelib
         @issuelib ||= class_for(:node).issue_library
+      end
+
+      def try_rescue_from_length_validation(args={})
+        model = args[:model]
+        field = args[:field]
+        text  = args[:text]
+        msg   = args[:msg]
+
+        logger.error{ "Trying to rescue from a :length error" }
+
+        if model.errors[field]
+          # the plugin tried to store too much information
+          msg = "#[Title]#\nTruncation warning!\n\n"
+          msg << "#[Error]#\np(alert alert-error). The plugin tried to store content that was too big for the DB. Review the source to ensure no important data was lost.\n\n"
+          msg << text
+          model.send("#{field}=", msg.truncate(65300))
+        else
+          # bail
+          msg = "#[Title]#\n#{msg}\n\n"
+          msg << "#[Description]#\nbc. #{issue.errors.inspect}\n\n"
+          model.send("#{field}=", msg)
+        end
+        if model.valid?
+          model.class.observers.disable :revision_observer
+          model.save
+          model.class.observers.enable :revision_observer
+        end
       end
     end
   end
