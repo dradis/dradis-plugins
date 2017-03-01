dce1caa6741d985c5c9987d31620b1a756c8988c
Daniel Martin <etd-github@nomejortu.com>
2017-03-01 09:03:37 +0100

    Export::BaseController - make an #export_options available to plugins

--- app/controllers/dradis/plugins/export/base_controller.rb
+++ app/controllers/dradis/plugins/export/base_controller.rb
@@ -3,6 +3,15 @@ module Dradis
     module Export
       class BaseController < Dradis::Plugins::base_export_controller_class.to_s.constantize
 
+        protected
+
+        # Protected: allows export plugins to access the options sent from the
+        # framework via the session object (see Export#create).
+        #
+        # Returns a Hash with indifferent access.
+        def export_options
+          @export_options ||= session[:export_manager].with_indifferent_access
+        end
       end
     end
   end
