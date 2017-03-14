4442118a0c6598f19ccbd329644ab559d47b09cc
Daniel Martin <etd-github@nomejortu.com>
2017-03-14 15:52:09 +0100

    Upload::Importer - detect plugin and save framework options

--- lib/dradis/plugins/upload/importer.rb
+++ lib/dradis/plugins/upload/importer.rb
@@ -5,11 +5,14 @@ module Dradis
   module Plugins
     module Upload
       class Importer
-        attr_accessor :content_service, :logger, :plugin, :template_service
+        attr_accessor :content_service, :logger, :options, :plugin, :project, :template_service
 
         def initialize(args={})
-          @plugin = args.fetch(:plugin)
-          @logger = args.fetch(:logger, Rails.logger)
+          @options = args
+
+          @logger  = args.fetch(:logger, Rails.logger)
+          @plugin  = args[:plugin] || default_plugin
+          @project = args.key?(:project_id) ? Project.find(args[:project_id]) : nil
 
           @content_service  = args.fetch(:content_service, default_content_service)
           @template_service = args.fetch(:template_service, default_template_service)
@@ -29,9 +32,29 @@ module Dradis
         def default_content_service
           @content ||= Dradis::Plugins::ContentService::Base.new(
             logger: logger,
-            plugin: plugin
+            plugin: plugin,
+            project: project
           )
         end
+
+        # This assumes the plugin's Importer class is directly nexted into the
+        # plugin's namespace (e.g. Dradis::Plugins::Nessus::Importer)
+        def default_plugin
+          plugin_module   = self.class.name.deconstantize
+          plugin_constant = plugin_module.constantize
+
+          if defined?(plugin_constant::Engine)
+            plugin_engine   = plugin_constant::Engine
+            if Dradis::Plugins.registered?(plugin_engine)
+              plugin_constant
+            else
+              raise "Your plugin isn't registered with the framework."
+            end
+          else
+            raise "You need to pass a :plugin value to your Importer or define it under your plugin's root namespace."
+          end
+        end
+
 
         def default_template_service
           @template ||= Dradis::Plugins::TemplateService.new(
