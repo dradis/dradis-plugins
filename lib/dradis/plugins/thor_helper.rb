0bd53244e4c66329175c31acbcfa10442a85a174
Aaron Manaloto <aaronpomanaloto@gmail.com>
2017-05-11 09:42:52 +0800

    Add task_options and logger to thor_helper

--- lib/dradis/plugins/thor_helper.rb
+++ lib/dradis/plugins/thor_helper.rb
@@ -2,8 +2,27 @@ module Dradis
   module Plugins
     # Helper methods for plugin Thor tasks
     module ThorHelper
+      attr_accessor :task_options, :logger
+
       def detect_and_set_project_scope
         ;
+      end
+
+      def task_options
+        @task_options ||= {logger: logger}
+      end
+
+      def logger
+        @logger ||= default_logger
+      end
+
+
+      private
+      def default_logger
+        STDOUT.sync   = true
+        logger        = Logger.new(STDOUT)
+        logger.level  = Logger::DEBUG
+        logger
       end
     end
   end
