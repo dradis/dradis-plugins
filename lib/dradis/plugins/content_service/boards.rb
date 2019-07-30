18d8fcefcd433f3f9609bd797d436333a406be41
Aaron Manaloto <aaronpomanaloto@gmail.com>
2019-07-30 15:22:12 +0800

    Use a method for the default node id

--- lib/dradis/plugins/content_service/boards.rb
+++ lib/dradis/plugins/content_service/boards.rb
@@ -8,13 +8,17 @@ module Dradis::Plugins::ContentService
 
     def create_board(args={})
       name    = args.fetch(:name, default_board_name)
-      node_id = args.fetch(:node_id, project.methodology_library.id)
+      node_id = args.fetch(:node_id, default_node_id)
       Board.create(name: name, project_id: project.id, node_id: node_id)
     end
 
     private
     def default_board_name
       "create_board() invoked by #{plugin} without a :name parameter"
+    end
+
+    def default_node_id
+      project.methodology_library.id
     end
   end
 end
