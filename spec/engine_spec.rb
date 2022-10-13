9bf50c457b8c6d83fe6f80dec99cc6ed2b87cbc3
Alex Rupom Hasdak <alexrupom@hotmail.com>
2022-10-13 20:12:17 +0600

    added specs for enable? enable! disable!

---
+++ spec/engine_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+class TestEngine < ::Rails::Engine
+  include ::Dradis::Plugins::Base
+  addon_settings :test_engine do
+  end
+end
+
+describe Dradis::Plugins::Base do
+  describe '#enabled?' do
+    it 'returns default value' do
+      expect(TestEngine.enabled?).to eq(false)
+    end
+  end
+  describe '#enable!' do
+    it 'sets enabled to true' do
+      expect { TestEngine.enable! }.to change {
+        TestEngine.enabled?
+      }.from(false).to(true)
+    end
+  end
+  describe '#disalbe!' do
+    it 'sets enabled to false' do
+      TestEngine.enable!
+      expect { TestEngine.disable! }.to change {
+        TestEngine.enabled?
+      }.from(true).to(false)
+    end
+  end
+end
