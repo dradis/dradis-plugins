b461ed634a6803db1f1a6387b9a325af4304c58d
Aaron Manaloto <mail@aapomm.io>
2023-03-14 17:44:09 +0800

    Add state for importer and content service

--- lib/dradis/plugins/thor_helper.rb
+++ lib/dradis/plugins/thor_helper.rb
@@ -9,7 +9,7 @@ module Dradis
       end
 
       def task_options
-        @task_options ||= { logger: logger }
+        @task_options ||= { logger: logger, state: :draft }
       end
 
       def logger
