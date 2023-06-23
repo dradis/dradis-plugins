990602c4cb919842c54d589d8dc26fd2dfb62195
Caitlin <caitlinhenry.ss@gmail.com>
2023-06-23 14:00:48 -0400

    accept image as part of dradis::plugins::import::result

--- lib/dradis/plugins/import/result.rb
+++ lib/dradis/plugins/import/result.rb
@@ -1,12 +1,13 @@
 module Dradis::Plugins::Import
   class Result
-    attr_accessor :description, :id, :tags, :title
+    attr_accessor :description, :id, :tags, :title, :images
 
     def initialize(args={})
       @description = args[:description] || "The Import plugin didn't provide a :description for this result."
       @id          = args[:id]          || "The Import plugin didn't provide an :id for this result."
       @tags        = args[:tags]        || []
       @title       = args[:title]       || "The Import plugin didn't provide a :title for this result."
+      @images      = args[:images]      || []
     end
   end
 end
