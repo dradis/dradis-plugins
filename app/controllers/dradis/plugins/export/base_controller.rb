ba69bd0ba1e3a3132f1bc0e47b990ec9a71ba80d
Aaron Manaloto <mail@aapomm.io>
2023-07-03 20:24:21 +0800

    Add template validation to base

--- app/controllers/dradis/plugins/export/base_controller.rb
+++ app/controllers/dradis/plugins/export/base_controller.rb
@@ -3,6 +3,7 @@ module Dradis
     module Export
       class BaseController < Rails.application.config.dradis.base_export_controller_class_name.to_s.constantize
         before_action :validate_scope
+        before_action :validate_template
 
         protected
 
@@ -10,10 +11,29 @@ module Dradis
           params.permit(:project_id, :scope, :template)
         end
 
-        def validate_scope
-          unless Dradis::Plugins::ContentService::Base::VALID_SCOPES.include?(params[:scope])
+        def validate_template
+          @template_file =
+            File.expand_path(File.join(templates_dir, export_params[:template]))
+
+          unless @template_file.starts_with?(templates_dir) && File.exists?(@template_file)
             raise 'Something fishy is going on...'
           end
+        end
+
+        def validate_scope
+          unless Dradis::Plugins::ContentService::Base::VALID_SCOPES.include?(export_params[:scope])
+            raise 'Something fishy is going on...'
+          end
+        end
+
+        private
+
+        def engine_name
+          "#{self.class.to_s.deconstantize}::Engine".constantize.plugin_name.to_s
+        end
+
+        def templates_dir
+          @templates_dir ||= File.join(::Configuration::paths_templates_reports, engine_name)
         end
       end
     end
