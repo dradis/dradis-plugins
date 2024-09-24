b0c6640bb990f449bc03998f6c3dcac8b29ba41b
Aaron Manaloto <mail@aapomm.io>
2024-09-24 17:31:11 +0800

    Add callback attribute to import results

--- lib/dradis/plugins/import/result.rb
+++ lib/dradis/plugins/import/result.rb
@@ -1,8 +1,9 @@
 module Dradis::Plugins::Import
   class Result
-    attr_accessor :description, :id, :tags, :title
+    attr_accessor :callback, :description, :id, :tags, :title
 
     def initialize(args={})
+      @callback    = args[:callback]    || Proc.new {}
       @description = args[:description] || "The Import plugin didn't provide a :description for this result."
       @id          = args[:id]          || "The Import plugin didn't provide an :id for this result."
       @tags        = args[:tags]        || []
