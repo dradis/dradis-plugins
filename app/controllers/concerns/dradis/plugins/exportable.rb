ab024e5d03d9e69563397daa53d2ee58ec67bee9
Caitlin <caitlinhenry.ss@gmail.com>
2025-06-06 14:51:03 -0400

    Moved shared export functionality to a concern so it can be used by multiple export-related controllers

---
+++ app/controllers/concerns/dradis/plugins/exportable.rb
@@ -0,0 +1,38 @@
+module Dradis
+  module Plugins
+    module Exportable
+      extend ActiveSupport::Concern
+
+      included do
+        before_action :set_exporter, only: [:create]
+        before_action :validate_scope, only: [:create]
+        before_action :validate_template, only: [:create]
+      end
+
+      private
+
+      def validate_template
+        @template_file =
+          File.expand_path(File.join(templates_dir, export_params[:template]))
+
+        unless @template_file.starts_with?(templates_dir) && File.exists?(@template_file)
+          raise 'Something fishy is going on...'
+        end
+      end
+
+      def validate_scope
+        unless Dradis::Plugins::ContentService::Base::VALID_SCOPES.include?(export_params[:scope])
+          raise 'Something fishy is going on...'
+        end
+      end
+
+      def set_exporter
+        raise NotImplementedError
+      end
+
+      def templates_dir
+        @templates_dir ||= File.join(::Configuration::paths_templates_reports, @exporter)
+      end
+    end
+  end
+end
