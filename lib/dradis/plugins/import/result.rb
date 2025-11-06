7dfd309b13be36dd114cd42afcc85aa186789d3b
Gerald Celanji <geraldcelanji@gmail.com>
2025-11-06 14:37:51 +0200

    add state attribute to Result class for improved result management

--- lib/dradis/plugins/import/result.rb
+++ lib/dradis/plugins/import/result.rb
@@ -1,12 +1,13 @@
 module Dradis::Plugins::Import
   class Result
-    attr_accessor :description, :id, :tags, :title
+    attr_accessor :description, :id, :tags, :title, :state
 
     def initialize(args={})
       @description = args[:description] || "The Import plugin didn't provide a :description for this result."
       @id          = args[:id]          || "The Import plugin didn't provide an :id for this result."
       @tags        = args[:tags]        || []
       @title       = args[:title]       || "The Import plugin didn't provide a :title for this result."
+      @state       = args[:state]       || "The Import plugin didn't provide a :state for this result."
     end
   end
 end
